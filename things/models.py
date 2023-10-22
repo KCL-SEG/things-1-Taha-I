96530f6eac860b0f0a2953c3f1bc04dcb7e4ebd5
Taha-I <tahaislam1111@gmail.com>
2023-10-22 20:51:12 +0100

    Added parameters for model attributes

--- things/models.py
+++ things/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 from django.db.models import Model
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(Model):
-    name = models.CharField()
-    description = models.CharField()
-    quantity = models.IntegerField()
+    name = models.CharField(
+        unique=True,
+        blank=False,
+        max_length=30,
+    )
+    description = models.CharField(
+        unique=False,
+        blank=True,
+        max_length=120,
+    )
+    quantity = models.IntegerField(
+        unique=False,
+        validators=[MinValueValidator(0),MaxValueValidator(100)]
+    )
